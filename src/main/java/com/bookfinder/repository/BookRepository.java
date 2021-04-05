1976748c8eed70b24c2bd51fc5258dae8f05dcde
flaviusfilipas <flaviusfilipas@protonmail.com>
2021-04-05 18:22:24 +0300

    Added repositories

---
+++ src/main/java/com/bookfinder/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.bookfinder.repository;
+
+import com.bookfinder.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book,Integer> {
+}
---
+++ src/main/java/com/bookfinder/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.bookfinder.repository;
+
+import com.bookfinder.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+}
