463a0c53ebab6df320cd01a5673012474c90e8d1
flaviusfilipas <flaviusfilipas@protonmail.com>
2021-04-07 14:34:02 +0300

    Added methods for creating and updating an user

--- src/main/java/com/bookfinder/controller/UserResource.java
+++ src/main/java/com/bookfinder/controller/UserResource.java
@@ -1,8 +1,12 @@
 package com.bookfinder.controller;
 
+import com.bookfinder.dto.UserDTO;
 import com.bookfinder.service.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("/api/users")
@@ -11,5 +15,18 @@ public class UserResource {
 
     public UserResource(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(@RequestBody UserDTO userDTO) throws URISyntaxException {
+        userService.save(userDTO);
+        return ResponseEntity.created(new URI("/api/users/" + userDTO.getId()))
+                .build();
+    }
+
+    @PutMapping
+    public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO userDTO) throws URISyntaxException {
+        UserDTO updatedUser = userService.save(userDTO);
+        return ResponseEntity.ok(updatedUser);
     }
 }
--- src/main/java/com/bookfinder/service/UserService.java
+++ src/main/java/com/bookfinder/service/UserService.java
@@ -1,13 +1,23 @@
 package com.bookfinder.service;
 
+import com.bookfinder.domain.User;
+import com.bookfinder.dto.UserDTO;
+import com.bookfinder.mapper.UserMapper;
 import com.bookfinder.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
+    private final UserMapper userMapper;
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    public UserDTO save(UserDTO userDTO){
+        User entity = userMapper.toEntity(userDTO);
+        User savedUser = userRepository.save(entity);
+        return userMapper.toDto(savedUser);
     }
 }
